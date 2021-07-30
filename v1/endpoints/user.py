8956b70d0125a7045d893ebeb22495bf7359c020
ordidxzero <creator@hanyang.ac.kr>
2021-07-30 14:24:10 +0900

    endpoint 뼈대를 만듦

--- v1/endpoints/user.py
+++ v1/endpoints/user.py
@@ -4,6 +4,21 @@ from fastapi import APIRouter
 user_router = APIRouter()
 
 
-@user_router.get("/")
+@user_router.get("/", description="Userrrrr", summary="AAAAA")
 def read_user():
     return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@user_router.post("/register")
+def register_user():
+    return ""
+
+
+@user_router.post("/login")
+def login():
+    return ""
+
+
+@user_router.post("/edit")
+def edit_profile():
+    return ""
