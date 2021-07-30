686d5681af5b55f710f7ec4ef082de34520372fc
ordidxzero <creator@hanyang.ac.kr>
2021-07-30 13:31:15 +0900

    임시 endpoint 추가

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from core.settings import Settings
 from fastapi import FastAPI
+from core.db import db
 
 
 def create_app():
     app = FastAPI()
 
+    config = Settings.load()
+
+    db.init_app(app=app, **config)
+
+    @app.get("/")
+    def root():
+        return {"message": "Hello World"}
+
     return app
+
+
+app = create_app()
